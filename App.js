19630314fc543fdae96d733c799bb08d98b5e2fa
Hemapriya <hemapriyasampath@gmail.com>
2024-01-19 13:14:50 -0500

    Setting up User Posts

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   FlatList,
   SafeAreaView,
@@ -17,7 +17,7 @@ import UserStory from './components/UserStory/UserStory';
 const App = () => {
   const userStories = [
     {
-      firstName: 'Joseph',
+      firstName: 'Moulesh',
       id: 1,
       profileImage: require('./assets/images/default_profile.png'),
     },
@@ -63,6 +63,81 @@ const App = () => {
     },
   ];
 
+  const userPosts = [
+    {
+      firstName: 'Aaby',
+      lastName: 'Becker',
+      location: 'chennai',
+      likes: 12,
+      comment: 23,
+      bookmark: 4,
+      id: 1,
+    },
+    {
+      firstName: 'Ray',
+      lastName: 'Hendry',
+      location: 'chennai',
+      likes: 24,
+      comment: 2,
+      bookmark: 14,
+      id: 2,
+    },
+    {
+      firstName: 'Bay',
+      lastName: 'Hendry',
+      location: 'chei',
+      likes: 234,
+      comment: 22,
+      bookmark: 4,
+      id: 3,
+    },
+    {
+      firstName: 'Banu',
+      lastName: 'Chandra',
+      location: 'ooty',
+      likes: 878,
+      comment: 29,
+      bookmark: 5,
+      id: 4,
+    },
+    {
+      firstName: 'Mai',
+      lastName: 'Chdra',
+      location: 'Kemberly',
+      likes: 8781,
+      comment: 9,
+      bookmark: 52,
+      id: 5,
+    },
+  ];
+
+  const userStoriesPageSize = 4;
+  const [userStoriesCurrentPage, setUserStoriesCurrentPage] = useState(1);
+  const [userStoriesRenderedData, setUserStoriesRenderedData] = useState([]);
+  const [isLoadingUserStories, setIsLoadingUserStories] = useState(false);
+
+  const userPostsPageSize = 4;
+  const [userPostsCurrentPage, setUserPostsCurrentPage] = useState(1);
+  const [userPostsRenderedData, setUserPostsRenderedData] = useState([]);
+  const [isLoadingUserPosts, setIsLoadingUserPosts] = useState(false);
+
+  const pagination = (database, currentPage, pageSize) => {
+    console.log('currentPage', currentPage);
+    const startIndex = (currentPage - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    if (startIndex >= database.length) {
+      return [];
+    }
+    return database.slice(startIndex, endIndex);
+  };
+
+  useEffect(() => {
+    setIsLoadingUserStories(true);
+    const getInitialData = pagination(userStories, 1, userStoriesPageSize);
+    setUserStoriesRenderedData(getInitialData);
+    setIsLoadingUserStories(false);
+  }, []);
+
   console.log(userStories.map(item => item.profileImage));
   return (
     <SafeAreaView>
@@ -77,11 +152,29 @@ const App = () => {
       </View>
       <View style={globalStyle.userStoryContainer}>
         <FlatList
+          onEndReachedThreshold={0.5}
+          onEndReached={() => {
+            if (isLoadingUserStories) {
+              return;
+            }
+            setIsLoadingUserStories(true);
+            const contentToAppend = pagination(
+              userStories,
+              userStoriesCurrentPage + 1,
+              userStoriesPageSize,
+            );
+            if (contentToAppend.length > 0) {
+              setUserStoriesCurrentPage(userStoriesCurrentPage + 1);
+              setUserStoriesRenderedData(prev => [...prev, ...contentToAppend]);
+            }
+            setIsLoadingUserStories(false);
+          }}
           showsHorizontalScrollIndicator={false}
           horizontal={true}
-          data={userStories}
+          data={userStoriesRenderedData}
           renderItem={({item}) => (
             <UserStory
+              key={item.id}
               firstName={item.firstName}
               profileImage={item.profileImage}
             />
